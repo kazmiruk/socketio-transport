1346b724b11743c51ef4f795b2c4f3b88db0e00e
kazmiruk <kazmiruk.evgeniy@gmail.com>
2014-04-24 19:13:41 +0800

    session authenticated removed with hash checking

--- src/namespaces/base.py
+++ src/namespaces/base.py
@@ -1,12 +1,14 @@
+import json
 import logging
 from gevent.queue import Queue
+from hashlib import sha256
 
 from socketio.namespace import BaseNamespace as ParentNamespace
 from socketio.mixins import RoomsMixin, BroadcastMixin
 
-from interfaces.session import SessionStore
 from interfaces.jobs import JobWrapper
 from interfaces.register import Register
+import settings
 
 
 class BaseNamespace(ParentNamespace, RoomsMixin, BroadcastMixin):
@@ -35,10 +37,8 @@ class BaseNamespace(ParentNamespace, RoomsMixin, BroadcastMixin):
     def recv_message(self, message):
         logging.debug("MSG: {}".format(message))
 
-    def on_login(self, session_key):
-        user_id = SessionStore().load(session_key)
-
-        if user_id:
+    def on_login(self, user_id, hash_key):
+        if user_id and hash_key == self.__check_hash(user_id):
             self.user_id = user_id
             logging.debug("User {user_id} session confirmed".format(user_id=user_id))
 
@@ -55,11 +55,12 @@ class BaseNamespace(ParentNamespace, RoomsMixin, BroadcastMixin):
 
             self.emit("login", {"status": True})
         else:
-            logging.debug("Can't auth user with session key {session_key}".format(
-                session_key=session_key
-            ))
+            logging.debug("Can't auth user {user_id} and hash {hash}".format(user_id=user_id, hash=hash_key))
             self.recv_disconnect()
             self.emit("login", {"status": False})
+
+    def __check_hash(self, user_id):
+        return sha256(str(user_id) + "|" + settings.SECRET_KEY).hexdigest()
 
     def _outter_task_loop(self):
         task = self.queue.get()

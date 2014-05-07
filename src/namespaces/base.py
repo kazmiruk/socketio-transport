864081b2337ce5c6ff9182cfe86f59d90cacd3fb
kazmiruk <kazmiruk.evgeniy@gmail.com>
2014-05-07 15:08:27 +0800

    #604: using site_id for connect

--- src/namespaces/base.py
+++ src/namespaces/base.py
@@ -15,7 +15,9 @@ class BaseNamespace(ParentNamespace, RoomsMixin, BroadcastMixin):
     def __init__(self, *args, **kwargs):
         super(BaseNamespace, self).__init__(*args, **kwargs)
         self.user_id = None
+        self.site_id = None
         self.queue = None
+        self.queue_name = None
 
     def recv_connect(self):
         logging.debug("New user connected")
@@ -23,47 +25,64 @@ class BaseNamespace(ParentNamespace, RoomsMixin, BroadcastMixin):
     def recv_disconnect(self):
         logging.debug("User disconnected")
 
-        if self.user_id:
+        if self.user_id and self.site_id:
             register = Register()
-            if self.user_id in register.queue and \
-                    self.socket.sessid in register.queue[self.user_id]:
-                if len(register.queue[self.user_id]) == 1:
-                    del register.queue[self.user_id]
+
+            if self.queue_name and self.queue_name in register.queue and \
+                    self.socket.sessid in register.queue[self.queue_name]:
+                if len(register.queue[self.queue_name]) == 1:
+                    del register.queue[self.queue_name]
                 else:
-                    del register.queue[self.user_id][self.socket.sessid]
+                    del register.queue[self.queue_name][self.socket.sessid]
 
         super(BaseNamespace, self).recv_disconnect()
 
     def recv_message(self, message):
         logging.debug("MSG: {}".format(message))
 
-    def on_login(self, user_id, hash_key):
-        if user_id and hash_key == self.__check_hash(user_id):
+    def on_login(self, user_id, site_id, hash_key):
+        if user_id and site_id and hash_key == self.__check_hash(user_id, site_id):
             if not isinstance(user_id, long):
                 user_id = long(user_id)
 
+            if not isinstance(site_id, long):
+                site_id = long(site_id)
+
             self.user_id = user_id
-            logging.debug("User {user_id} session confirmed".format(user_id=user_id))
+            self.site_id = site_id
+            logging.debug("User {user_id} session for site {site_id} confirmed".format(
+                user_id=user_id,
+                site_id=site_id
+            ))
 
             self.socket.session['user_id'] = user_id
+            self.socket.session['site_id'] = site_id
 
             self.queue = Queue(maxsize=None)
             register = Register()
 
-            if user_id not in register.queue:
-                register.queue[user_id] = {}
+            self.queue_name = str(user_id) + "_" + str(site_id)
 
-            register.queue[user_id][self.socket.sessid] = self.queue
+            if self.queue_name not in register.queue:
+                register.queue[self.queue_name] = {}
+
+            register.queue[self.queue_name][self.socket.sessid] = self.queue
             JobWrapper.start_listen(self._outter_task_loop)
 
             self.emit("login", {"status": True})
         else:
-            logging.debug("Can't auth user {user_id} and hash {hash}".format(user_id=user_id, hash=hash_key))
+            logging.debug("Can't auth user {user_id} on site {site} and hash {hash}".format(
+                user_id=user_id,
+                site=site_id,
+                hash=hash_key
+            ))
             self.recv_disconnect()
             self.emit("login", {"status": False})
 
-    def __check_hash(self, user_id):
-        return sha256(str(user_id) + "|" + settings.SECRET_KEY).hexdigest()
+    def __check_hash(self, user_id, site_id):
+        return sha256(
+            str(user_id) + "|" + str(site_id) + "|" + settings.SECRET_KEY
+        ).hexdigest()
 
     def _outter_task_loop(self):
         task = self.queue.get()
@@ -71,7 +90,8 @@ class BaseNamespace(ParentNamespace, RoomsMixin, BroadcastMixin):
             task.do(self)
             return True
 
-        logging.debug("Task loop finished for user {user_id}".format(
-            user_id=self.user_id
+        logging.debug("Task loop finished for user {user_id} and site {site_id}".format(
+            user_id=self.user_id,
+            site_id=self.site_id
         ))
         return False
